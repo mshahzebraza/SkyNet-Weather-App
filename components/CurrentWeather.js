241a6e9d4c2fb924270bba2663bbd80eb5641b7f
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2021-10-27 22:41:12 +0500

    Troubleshooting
    The code in comments did not work.

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,18 +1,72 @@
+import { useState, useEffect } from "react";
 import useStore from "../store/StoreContext"
 
-export default async function CurrentWeather(props) {
-  const { state } = useStore()
-  // const { currentLocation } = state;
+export default function CurrentWeather(props) {
+
+  // const [weatherData, setWeatherData] = useState({});
+
+  const fetchHandler = async () => {
+    console.log(`i was called`);
+
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=london&aqi=yes`)
+    const data = await response.json();
+    console.log(data);
+
+  }
 
 
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=${currentLocation}&aqi=yes`
-  );
-  // const data = await response.json()
-  // console.log(data);
 
   return (
     <div className='card'>
-      {props.info}
+      <button onClick={fetchHandler} >Click me</button>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+export default function CurrentWeather(props) {
+  const apiKey = `df0dcf32a9b346308a814745212710`;
+
+  const { state } = useStore()
+  const { currentLocation } = state;
+
+  const weatherConfig = {
+    location: currentLocation,
+    apiKey,
+    aqReq: true
+  }
+
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=${config.location}&aqi=yes`)
+      .then((response) => {
+        return response.json()
+      }).then((data) => {
+        console.log(data);
+      });
+
+
+  }, [weatherConfig.location])
+
+
+
+  return (
+    <div className='card'>
+      {weatherData}
+    </div>
+  )
+}
+ */
