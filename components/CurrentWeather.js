608012c5b5fe76b05a4e0c219631057ea391f30f
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2021-10-27 22:50:56 +0500

    useEffect & useCallback
    without the useEffect hook the fetch function is called twice. see log
    don't understand why?
    
    But the useCallback is necesary to avoid the recreation of the fetchHandler instance on every render which starts a loop by re-running the useEffect (beacause it is a dependancy of useEffect)

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,19 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import useStore from "../store/StoreContext"
 
 export default function CurrentWeather(props) {
 
   // const [weatherData, setWeatherData] = useState({});
 
-  const fetchHandler = async () => {
+
+  const fetchHandler = useCallback(async () => {
     console.log(`i was called`);
 
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=london&aqi=yes`)
     const data = await response.json();
     console.log(data);
 
-  }
+  })
 
+
+  useEffect(() => {
+    fetchHandler()
+  }, [fetchHandler])
+  // The fetchHandler dependency is a reference object hence the use of useCallback is understandable bcz a new instance of an object is created on every render. 
 
 
   return (
