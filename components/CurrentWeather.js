4a011321be889fdc9f26517ee49df60f7bd9b0e9
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2021-10-28 14:57:13 +0500

    fetch Data from fetch Calls
    You can not just call a fetch function inside a setState call and expected the result of fetch to be returned to setState and stored in it eventually.
    Instead, you use the setState function inside the fetchHandler function and store the data in it yourself. i.e
    
    WRONG: setState( fetchHandler(apiKey) )
    CORRECT:
    fetchHandler() {
    ...  data
        setState(data )
    }
    
    Ofcourse if you use the setState in a fetchHandler, then you have to pay attention if the fetchHandler is inside the useCallback or not. If it is wrapped in a callback then pass the setState as a dependancy of callback

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -3,35 +3,71 @@ import useStore from "../store/StoreContext"
 
 export default function CurrentWeather(props) {
 
-  // const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({});
 
 
   const fetchHandler = useCallback(async () => {
-    console.log(`i was called`);
+    console.log(`fetchHandler was called`);
 
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=london&aqi=yes`)
     const data = await response.json();
     console.log(data);
+    setWeatherData(data)
+    console.log(data);
 
-  })
+  }, [setWeatherData])
 
 
   useEffect(() => {
     fetchHandler()
+    // setWeatherData(fetchHandler())
   }, [fetchHandler])
   // The fetchHandler dependency is a reference object hence the use of useCallback is understandable bcz a new instance of an object is created on every render. 
 
 
   return (
     <div className='card'>
-      <button onClick={fetchHandler} >Click me</button>
+      <div>
+        Weather Data here
+        {/* 
+        WeatherConditions in {location.name} ({location.country}) is as follow.
+
+        1. Weather Conditions: {current.condition.text}
+        2. Temperature (Actual) : {current.temp_c}
+        3. Temperature (Feels Like): {current.feelslike_c}
+        4. Wind Direction: {current.wind_dir}
+        5. Wind Speed: {current.wind_kph}
+        6. Humidity: {current.humidity}
+
+        Daytime/NightTime (can be used to display the graphics accordingly)
+
+        - This data was fetched on [Date] at [time].
+       */}
+        {/* - {current.is_day ? `Day` : `Night`} */}
+
+      </div>
+      <button onClick={fetchHandler} >Click me to reload Data</button>
     </div>
   )
 }
 
+CurrentWeather.displayName = `CurrentWeather`;
+
+/*
+WeatherConditions in London (United Kingdom) is as follow.
+1. Weather Conditions: current.condition.text
+2. Temperature (Actual) : current.temp_c
+3. Temperature (Feels Like): current.feelslike_c
+4. Wind Direction: current.wind_dir
+5. Wind Speed: current.wind_kph
+6. Humidity: current.humidity
+
+Daytime/NightTime (can be used to display the graphics accordingly)
 
 
 
+- This data was fetched on [Date] at [time]
+ */
 
 
 
