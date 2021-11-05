fa79757c74388b453edf09f58760daee046bc991
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2021-11-05 22:48:15 +0500

    name change

--- pages/api/newLocationInput.js
+++ pages/api/newLocationInput.js
@@ -6,24 +6,24 @@ export default function handler(req, res) {
 
   // const fetchHandler = useCallback(async (location = `Washington`) => {
 
-  try {
+  // try {
 
-    console.log(`fetch Running ...`);
-    console.log(`fetch Location: ${location}`);
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=${location}&aqi=yes`)
+  //   console.log(`fetch Running ...`);
+  //   console.log(`fetch Location: ${location}`);
+  //   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=${location}&aqi=yes`)
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      dispatch(searchCurrent(data))
-    } else
-      if (!response.ok) {
-        const errorData = await response.json()
-        const { code: errCode, message: errMsg } = errorData.error
-        throw new Error(errMsg);
-      }
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     console.log(data);
+  //     dispatch(searchCurrent(data))
+  //   } else
+  //     if (!response.ok) {
+  //       const errorData = await response.json()
+  //       const { code: errCode, message: errMsg } = errorData.error
+  //       throw new Error(errMsg);
+  //     }
 
-  } catch (error) {
-    console.error(error);
-  }
+  // } catch (error) {
+  //   console.error(error);
+  // }
 }
