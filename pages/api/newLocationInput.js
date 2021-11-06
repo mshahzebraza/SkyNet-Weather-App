d8632cf119d2acea5cee338ed486952926dee9dd
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2021-11-06 14:38:27 +0500

    Update newLocationInput.js

--- pages/api/newLocationInput.js
+++ pages/api/newLocationInput.js
@@ -4,26 +4,4 @@ export default function handler(req, res) {
 
   res.status(200).json({ name: 'John Doe' })
 
-  // const fetchHandler = useCallback(async (location = `Washington`) => {
-
-  try {
-
-    console.log(`fetch Running ...`);
-    console.log(`fetch Location: ${location}`);
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=df0dcf32a9b346308a814745212710&q=${location}&aqi=yes`)
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      dispatch(searchCurrent(data))
-    } else
-      if (!response.ok) {
-        const errorData = await response.json()
-        const { code: errCode, message: errMsg } = errorData.error
-        throw new Error(errMsg);
-      }
-
-  } catch (error) {
-    console.error(error);
-  }
 }
